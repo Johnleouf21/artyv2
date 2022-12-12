5f28219677b179e1e6e907e69d840fb06b679c54
Johnleouf21 <charlot.jonathn@hotmail.fr>
2022-12-12 21:38:39 +0100

    new

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="/css/loader.css" media="screen"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+        <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Roboto+Mono:wght@600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Joan&family=Poppins&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
